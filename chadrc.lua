09353e972b738c2f4dbd64ea0da4276c76eb235f
haod2s <holden_s@163.com>
2023-04-29 01:07:48 +0800

    refactor: remove useless code

--- chadrc.lua
+++ chadrc.lua
@@ -3,4 +3,11 @@ local M = {}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
+-- seem not work
+M.ui = {
+    hl_override = {
+        CursorLine = { bg = "one_bg" }
+    }
+}
+
 return M
