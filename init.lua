5be31803795455631894adb5dedbe124e865105d
haod2s <holden_s@163.com>
2023-05-25 02:19:45 +0800

    feat(filetype): add different settings for lua, help, quickfix

--- init.lua
+++ init.lua
@@ -3,3 +3,20 @@ vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 
 vim.wo.colorcolumn = "120"
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("FileType", {
+  pattern = "help,qf",
+  callback = function()
+    vim.wo.colorcolumn = "0"
+  end,
+})
+
+autocmd("FileType", {
+  pattern = "lua",
+  callback = function()
+    vim.o.softtabstop = 2
+    vim.o.shiftwidth = 2
+  end,
+})
